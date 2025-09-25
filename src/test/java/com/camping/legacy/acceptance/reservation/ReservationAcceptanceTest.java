f0cb97c2ad23ed58f72b880e10c1e1c68e24f883
brucehan <intragerhan@gmail.com>
2025-09-25 21:52:33 +0900

    fix : 피드백 반영

--- src/test/java/com/camping/legacy/acceptance/reservation/ReservationAcceptanceTest.java
+++ src/test/java/com/camping/legacy/acceptance/reservation/ReservationAcceptanceTest.java
@@ -22,11 +22,10 @@ public class ReservationAcceptanceTest extends AcceptanceTestBase {
          * 무엇을 : 30일내로 비어있는 날짜에 대한 예약을
          * 왜 : 유효한 정보로 예약에 성공하기 위해
          */
-        // given - 오늘 날짜를 기준으로 30일 이내의 날짜를 계산하고
+        // when - 30일 이내 날짜로 예약을 신청했을 때
         ReservationTestDataBuilder builder = new ReservationTestDataBuilder();
         Map<String, String> requestMap = builder.withSiteNumber("B-1").buildRequestMap();
-
-        // when - 30일 이내 날짜의 예약을 신청했을 때
+        
         ExtractableResponse<Response> response = createReservation(requestMap);
 
         // then - 예약에 성공한다
@@ -43,11 +42,10 @@ public class ReservationAcceptanceTest extends AcceptanceTestBase {
          * 무엇을 : 31일 이후의 날짜 예약을
          * 왜 : 31일 이후의 날짜는 예약할 수 없다는 안내를 받기 위해
          */
-        // given - 오늘 날짜를 기준으로 30일 이후의 날짜를 계산하고
+        // when - 30일 이후 날짜로 예약을 신청했을 때
         ReservationTestDataBuilder builder = new ReservationTestDataBuilder();
         Map<String, String> requestMap = builder.withSiteNumber("B-1").withDatesInFuture(31, 32).buildRequestMap();
 
-        // when - 30일 이후 날짜의 예약을 신청했을 때
         ExtractableResponse<Response> response = createReservation(requestMap);
 
         // then - 예약에 실패하고, "30일 이후의 날짜는 예약할 수 없다는 안내"를 받는다.
@@ -67,11 +65,12 @@ public class ReservationAcceptanceTest extends AcceptanceTestBase {
         Long reservationId = 1L;
         ExtractableResponse<Response> previousReservation = getReservation(reservationId);
 
+        // when - 예약을 수정했을 때
         ReservationTestDataBuilder builder = new ReservationTestDataBuilder();
         Map<String, String> afterRequest = builder.withSiteNumber("A-2").buildRequestMap();
+        String confirmationCode = previousReservation.jsonPath().getString("confirmationCode");
 
-        // when - 예약을 수정했을 때
-        ExtractableResponse<Response> response = updateReservation(reservationId, previousReservation.jsonPath().getString("confirmationCode"), afterRequest);
+        ExtractableResponse<Response> response = updateReservation(reservationId, confirmationCode, afterRequest);
 
         // then - 예약 수정에 성공한다
         assertUpdatedSuccessfully(response);
@@ -90,11 +89,12 @@ public class ReservationAcceptanceTest extends AcceptanceTestBase {
         Long reservationId = 1L;
         ExtractableResponse<Response> previousReservation = getReservation(reservationId);
 
+        // when - 예약을 수정했을 때
         ReservationTestDataBuilder builder = new ReservationTestDataBuilder();
         Map<String, String> afterRequest = builder.withSiteNumber("X-999").buildRequestMap();
+        String confirmationCode = previousReservation.jsonPath().getString("confirmationCode");
 
-        // when - 예약을 수정했을 때
-        ExtractableResponse<Response> response = updateReservation(reservationId, previousReservation.jsonPath().getString("confirmationCode"), afterRequest);
+        ExtractableResponse<Response> response = updateReservation(reservationId, confirmationCode, afterRequest);
 
         // then - 예약에 실패하고, "존재하지 않는 캠핑장입니다"라는 안내를 받는다.
         assertBadRequestWithMessage(response, "존재하지 않는 캠핑장입니다.");
